672904feae74f6c0c974d5327f372d07a5e296a5
ghimiresk <ghimiresushank08@gmail.com>
2024-12-05 21:57:11 +0545

    call charge amount updated.

--- Assignment 2/Ntc Call service.c
+++ Assignment 2/Ntc Call service.c
@@ -5,23 +5,32 @@ First 30 calls: Free
 Above 60 calls: Rs. 1 per call Write a program to find the amount paid by the customer,
  if the total number of calls is given.
 */
+#include <stdio.h>
 
-#include<stdio.h>
-
-int main() {
+int main()
+{
 	int calls;
-	float charge;
-	printf("Enter the number of calls : ");
+	float amount = 0;
+
+	// Input the total number of calls
+	printf("Enter the total number of calls: ");
 	scanf("%d", &calls);
-	
-	if(calls <= 30) {
-		printf("You are in free call package.");
-	} else if(calls > 30 && calls < 60) {
-		charge = (calls-30) * 0.5;
-		printf("Your total charge is Rs. %.2f.", charge);
-	} else {
-		charge = (calls-30);
-		printf("Your total charge is Rs. %.2f.", charge);
+
+	// Calculate the charge based on the number of calls
+	if (calls <= 30)
+	{
+		amount = 0; // First 30 calls are free
 	}
+	else if (calls <= 60)
+	{
+		amount = (calls - 30) * 0.50; // Next 31 to 60 calls cost Rs. 0.50 per call
+	}
+	else
+	{
+		amount = (30 * 0.50) + ((calls - 60) * 1); // First 31 to 60 calls + calls above 60
+	}
+
+	printf("The total amount to be paid is: Rs. %.2f\n", amount);
+
 	return 0;
 }
