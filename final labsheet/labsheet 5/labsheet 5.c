50202c6823844bfe94923c700ca19da2fd820f48
ghimiresk <ghimiresushank08@gmail.com>
2025-02-12 18:02:06 +0545

    Completed Labsheet 5 with all the programs

--- final labsheet/labsheet 5/labsheet 5.c
+++ final labsheet/labsheet 5/labsheet 5.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
+struct Airline
+{
+    int flightNumber;
+    char origin[4];
+    char destination[4];
+    char startTime[6];
+    char arrivalTime[6];
+};
 // Function Prototypes
 void question1()
 {
@@ -16,12 +24,12 @@ void question1()
         {"Gita", 130, 'B'},
         {"Gopal", 150, 'A'}};
 
-    printf("
-Name	Total Marks	Grade
-");
-    for (int i = 0; i < 4; i++) {
-        printf("%s	%d		%c
-", students[i].name, students[i].totalMarks, students[i].grade);
+    printf("Name | Mark | Grade");
+    int i;
+    for (i = 0; i < 4; i++)
+    {
+        printf("\n");
+        printf("%s	%d	%c", students[i].name, students[i].totalMarks, students[i].grade);
     }
 }
 
@@ -36,8 +44,7 @@ void question2()
         char remarks[100];
     } s;
 
-    printf("
-Enter Name: ");
+    printf("Enter Name: ");
     scanf("%s", s.name);
     printf("Enter Roll: ");
     scanf("%d", &s.roll);
@@ -46,18 +53,13 @@ Enter Name: ");
     printf("Enter Remarks: ");
     scanf("%s", s.remarks);
 
-    printf("
-Student Details:
-");
-    printf("Name: %s
-Roll: %d
-Marks: %.2f
-Remarks: %s
-", s.name, s.roll, s.marks, s.remarks);
+    printf("Student Details:");
+    printf("Name: %s Roll: %d Marks: %.2f Remarks: %s ", s.name, s.roll, s.marks, s.remarks);
 }
 
 void question3()
 {
+    int i, j;
     // Question 3: Array of structure for 5 students with marks > 50
     struct Student
     {
@@ -66,10 +68,9 @@ void question3()
         float marks;
     } students[5];
 
-    for (int i = 0; i < 5; i++)
+    for (i = 0; i < 5; i++)
     {
-        printf("
-Enter Name of Student %d: ", i + 1);
+        printf("Enter Name of Student %d: ", i + 1);
         scanf("%s", students[i].name);
         printf("Enter Roll: ");
         scanf("%d", &students[i].roll);
@@ -77,14 +78,12 @@ Enter Name of Student %d: ", i + 1);
         scanf("%f", &students[i].marks);
     }
 
-    printf("
-Students with marks greater than 50:
-");
-    for (int i = 0; i < 5; i++) {
+    printf("Students with marks greater than 50: ");
+    for (i = 0; i < 5; i++)
+    {
         if (students[i].marks > 50)
         {
-            printf("Name: %s, Roll: %d
-", students[i].name, students[i].roll);
+            printf("Name: %s, Roll: %d", students[i].name, students[i].roll);
         }
     }
 }
@@ -99,10 +98,11 @@ void question4()
         float salary;
     } employees[5], temp;
 
-    for (int i = 0; i < 5; i++)
+    int i, j;
+
+    for (i = 0; i < 5; i++)
     {
-        printf("
-Enter Name of Employee %d: ", i + 1);
+        printf("Enter Name of Employee %d: ", i + 1);
         scanf("%s", employees[i].name);
         printf("Enter Address: ");
         scanf("%s", employees[i].address);
@@ -111,9 +111,9 @@ Enter Name of Employee %d: ", i + 1);
     }
 
     // Sorting by name
-    for (int i = 0; i < 4; i++)
+    for (i = 0; i < 4; i++)
     {
-        for (int j = i + 1; j < 5; j++)
+        for (j = i + 1; j < 5; j++)
         {
             if (strcmp(employees[i].name, employees[j].name) > 0)
             {
@@ -124,17 +124,17 @@ Enter Name of Employee %d: ", i + 1);
         }
     }
 
-    printf("
-Employees in Ascending Order of Name:
-");
-    for (int i = 0; i < 5; i++) {
-        printf("Name: %s, Address: %s, Salary: %.2f
-", employees[i].name, employees[i].address, employees[i].salary);
+    printf(" Employees in Ascending Order of Name: ");
+    for (i = 0; i < 5; i++)
+    {
+        printf("\n");
+        printf("Name: %s, Address: %s, Salary: %.2f ", employees[i].name, employees[i].address, employees[i].salary);
     }
 }
 
 void question5()
 {
+    int i, j;
     // Question 5: Sort names in alphabetical order using structure
     struct Person
     {
@@ -142,19 +142,18 @@ void question5()
         char address[100];
     } people[5], temp;
 
-    for (int i = 0; i < 5; i++)
+    for (i = 0; i < 5; i++)
     {
-        printf("
-Enter Name of Person %d: ", i + 1);
+        printf("Enter Name of Person %d: ", i + 1);
         scanf("%s", people[i].name);
         printf("Enter Address: ");
         scanf("%s", people[i].address);
     }
 
     // Sorting names alphabetically
-    for (int i = 0; i < 4; i++)
+    for (i = 0; i < 4; i++)
     {
-        for (int j = i + 1; j < 5; j++)
+        for (j = i + 1; j < 5; j++)
         {
             if (strcmp(people[i].name, people[j].name) > 0)
             {
@@ -165,12 +164,10 @@ Enter Name of Person %d: ", i + 1);
         }
     }
 
-    printf("
-Names in Alphabetical Order:
-");
-    for (int i = 0; i < 5; i++) {
-        printf("Name: %s, Address: %s
-", people[i].name, people[i].address);
+    printf("Names in Alphabetical Order: ");
+    for (i = 0; i < 5; i++)
+    {
+        printf("\nName: %s, Address: %s ", people[i].name, people[i].address);
     }
 }
 
@@ -186,22 +183,17 @@ void question6()
 
     ptr = &b;
 
-    printf("
-Enter Book Name: ");
+    printf("Enter Book Name: ");
     scanf("%s", ptr->name);
     printf("Enter Number of Pages: ");
     scanf("%d", &ptr->pages);
     printf("Enter Price: ");
     scanf("%f", &ptr->price);
 
-    printf("
-Book Details:
-");
-    printf("Name: %s
-Pages: %d
-Price: %.2f
-", ptr->name, ptr->pages, ptr->price);
+    printf("Book Details: ");
+    printf("Name: %s Pages: %d Price: %.2f ", ptr->name, ptr->pages, ptr->price);
 }
+
 void question7()
 {
     // Question 7: Array of structure to find most expensive book
@@ -212,10 +204,10 @@ void question7()
         float price;
     } books[5];
 
-    for (int i = 0; i < 5; i++)
+    int i, j;
+    for (i = 0; i < 5; i++)
     {
-        printf("
-Enter Book Name %d: ", i + 1);
+        printf("Enter Book Name %d: ", i + 1);
         scanf("%s", books[i].name);
         printf("Enter Number of Pages: ");
         scanf("%d", &books[i].pages);
@@ -225,7 +217,7 @@ Enter Book Name %d: ", i + 1);
 
     // Find most expensive book
     int maxIndex = 0;
-    for (int i = 1; i < 5; i++)
+    for (i = 1; i < 5; i++)
     {
         if (books[i].price > books[maxIndex].price)
         {
@@ -233,17 +225,13 @@ Enter Book Name %d: ", i + 1);
         }
     }
 
-    printf("
-Most Expensive Book:
-");
-    printf("Name: %s
-Pages: %d
-Price: %.2f
-", books[maxIndex].name, books[maxIndex].pages, books[maxIndex].price);
+    printf("Most Expensive Book: ");
+    printf("Name: %s Pages: %d Price: %.2f", books[maxIndex].name, books[maxIndex].pages, books[maxIndex].price);
 }
 
 void question8()
 {
+    int i, j;
     // Question 8: Union to display roll and marks one at a time
     union Student
     {
@@ -253,14 +241,11 @@ void question8()
 
     // Assign and display roll number
     s.roll = 101;
-    printf("
-Roll Number: %d
-", s.roll);
+    printf("Roll Number: %d ", s.roll);
 
     // Assign and display marks
     s.marks = 85.5;
-    printf("Marks: %.2f
-", s.marks);
+    printf("Marks: %.2f", s.marks);
 }
 
 void question9()
@@ -273,9 +258,7 @@ void question9()
         int year;
     } date1 = {25, "February", 1957};
 
-    printf("
-Date: %s %d, %d
-", date1.month, date1.day, date1.year);
+    printf("Date: %s %d, %d ", date1.month, date1.day, date1.year);
 }
 
 void question10()
@@ -288,17 +271,14 @@ void question10()
         int year;
     } date;
 
-    printf("
-Enter Day: ");
+    printf("Enter Day: ");
     scanf("%d", &date.day);
     printf("Enter Month: ");
     scanf("%d", &date.month);
     printf("Enter Year: ");
     scanf("%d", &date.year);
 
-    printf("
-Date: %02d/%02d/%d
-", date.day, date.month, date.year);
+    printf("Date: %02d/%02d/%d", date.day, date.month, date.year);
 }
 
 void question11()
@@ -311,21 +291,15 @@ void question11()
         int pin;
     } student;
 
-    printf("
-Enter Username (8 chars): ");
+    printf("Enter Username (8 chars): ");
     scanf("%s", student.username);
     printf("Enter ID Number (9 chars): ");
     scanf("%s", student.idNumber);
     printf("Enter 5-digit PIN: ");
     scanf("%d", &student.pin);
 
-    printf("
-Student Registration Details:
-");
-    printf("Username: %s
-ID Number: %s
-PIN: %d
-", student.username, student.idNumber, student.pin);
+    printf("Student Registration Details:");
+    printf("Username: %s ID Number: %s PIN: %d ", student.username, student.idNumber, student.pin);
 }
 
 void question12()
@@ -338,8 +312,7 @@ void question12()
         int year;
     } date1, date2;
 
-    printf("
-Enter First Date (dd mm yyyy): ");
+    printf("Enter First Date (dd mm yyyy): ");
     scanf("%d %d %d", &date1.day, &date1.month, &date1.year);
 
     printf("Enter Second Date (dd mm yyyy): ");
@@ -349,72 +322,32 @@ Enter First Date (dd mm yyyy): ");
     int monthDiff = date2.month - date1.month;
     int yearDiff = date2.year - date1.year;
 
-    if (dayDiff < 0) {
+    if (dayDiff < 0)
+    {
         dayDiff += 30;
         monthDiff--;
     }
-    if (monthDiff < 0) {
+    if (monthDiff < 0)
+    {
         monthDiff += 12;
         yearDiff--;
     }
 
-    printf("
-Difference: %d Years, %d Months, %d Days
-", yearDiff, monthDiff, dayDiff);
+    printf(" Difference: %d Years, %d Months, %d Days", yearDiff, monthDiff, dayDiff);
 }
 
-void question13()
+void flightTime()
 {
-    // Question 13: Structure for airline data
-    struct Airline
+    struct Airline flights[3];
+    char airportCode[4];
+    int i;
+
+    // Input Flight Details
+    printf("Enter details for 3 flights: ");
+    for (i = 0; i < 3; i++)
     {
-        int flightNumber;
-        char origin[4];
-        char destination[4];
-        char startTime[6];
-        char arrivalTime[6];
-    } flight;
-
-    printf("
-Enter Flight Number: ");
-    scanf("%d", &flight.flightNumber);
-    printf("Enter Origin Airport Code: ");
-    scanf("%s", flight.origin);
-    printf("Enter Destination Airport Code: ");
-    scanf("%s", flight.destination);
-    printf("Enter Start Time (HH:MM): ");
-    scanf("%s", flight.startTime);
-    printf("Enter Arrival Time (HH:MM): ");
-    scanf("%s", flight.arrivalTime);
-
-    printf("
-Flight Information:
-");
-    printf("Flight Number: %d
-Origin: %s
-Destination: %s
-Start Time: %s
-Arrival Time: %s
-",
-           flight.flightNumber, flight.origin, flight.destination, flight.startTime, flight.arrivalTime);
-}
-
-void question14()
-{
-    // Question 14: Print flight info for specific airport
-    struct Airline
-    {
-        int flightNumber;
-        char origin[4];
-        char destination[4];
-        char startTime[6];
-        char arrivalTime[6];
-    } flights[3];
-
-    for (int i = 0; i < 3; i++)
-    {
-        printf("
-Enter Flight Number: ");
+        printf("Flight %d: ", i + 1);
+        printf("Enter Flight Number: ");
         scanf("%d", &flights[i].flightNumber);
         printf("Enter Origin Airport Code: ");
         scanf("%s", flights[i].origin);
@@ -426,22 +359,23 @@ Enter Flight Number: ");
         scanf("%s", flights[i].arrivalTime);
     }
 
-    char airportCode[4];
-    printf("
-Enter Airport Code to Search: ");
+    // Display All Flight Information
+    printf("All Flight Information: ");
+    for (i = 0; i < 3; i++)
+    {
+        printf("Flight Number: %d | Origin: %s | Destination: %s | Start: %s | Arrival: %s", flights[i].flightNumber, flights[i].origin, flights[i].destination, flights[i].startTime, flights[i].arrivalTime);
+    }
+
+    // Search by Airport Code
+    printf("Enter Airport Code to Search: ");
     scanf("%s", airportCode);
 
-    printf("
-Flights for Airport %s:
-", airportCode);
-    for (int i = 0; i < 3; i++) {
+    printf("Flights for Airport %s: ", airportCode);
+    for (i = 0; i < 3; i++)
+    {
         if (strcmp(flights[i].origin, airportCode) == 0 || strcmp(flights[i].destination, airportCode) == 0)
         {
-            printf("Flight Number: %d, Origin: %s, Destination: %s, Start: %s, Arrival: %s
-                   ",
-                   flights[i]
-                       .flightNumber,
-                   flights[i].origin, flights[i].destination, flights[i].startTime, flights[i].arrivalTime);
+            printf("Flight Number: %d | Origin: %s | Destination: %s | Start: %s | Arrival: %s", flights[i].flightNumber, flights[i].origin, flights[i].destination, flights[i].startTime, flights[i].arrivalTime);
         }
     }
 }
@@ -463,33 +397,26 @@ void question15()
         struct Date hireDate;
     } person1 = {"Hari", 50000.0, hireDate};
 
-    printf("
-Employee Details:
-");
-    printf("Name: %s
-Salary: $%.2f
-Hire Date: %s %d, %d
-",
-           person1.name, person1.salary, person1.hireDate.month, person1.hireDate.day, person1.hireDate.year);
+    printf("Employee Details: ");
+    printf("Name: %s Salary: $%.2f Hire Date: %s %d, %d", person1.name, person1.salary, person1.hireDate.month, person1.hireDate.day, person1.hireDate.year);
 }
 
 int main()
 {
-    question1();
-    // question2();
-    // question3();
-    // question4();
-    // question5();
-    // question6();
-    // question7();
-    // question8();
-    // question9();
-    // question10();
-    // question11();
-    // question12();
-    // question13();
-    // question14();
-    // question15();
+    //    question1();
+    //     question2();
+    //     question3();
+    //     question4();
+    //     question5();
+    //     question6();
+    //     question7();
+    //     question8();
+    //     question9();
+    //     question10();
+    //     question11();
+    //     question12();
+    //	   flightTime();
+    question15();
 
     return 0;
 }
